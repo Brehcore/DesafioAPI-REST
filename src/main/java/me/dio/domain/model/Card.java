7e9b0f8cf631379fae3cda10a6c083356e5205c1
BrehCore <brehcore@gmail.com>
2023-10-20 17:18:33 -0300

    first commit

---
+++ src/main/java/me/dio/domain/model/Card.java
@@ -0,0 +1,49 @@
+package me.dio.domain.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "tb_card")
+public class Card {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+	
+	@Column(unique = true)
+    private String cardNumber;
+    
+    @Column(scale = 2, precision = 13)
+    private BigDecimal cardLimit;
+
+    // Getters and setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public BigDecimal getCardLimit() {
+        return cardLimit;
+    }
+
+    public void setCardLimit(BigDecimal cardLimit) {
+        this.cardLimit = cardLimit;
+    }
+}
+
